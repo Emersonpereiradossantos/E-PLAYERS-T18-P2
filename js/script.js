3e05c935c46568e8906f3881b6128db013e5a937
Emerson <96029393+Emersonpereiradossantos@users.noreply.github.com>
2023-04-01 19:25:14 -0300

    Terceiro commit-js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//alert("esta funcionando o js")
+
+function cadastrarNewsletter(){
+  let email= document.getElementById("campo-email").Value
+  alert('email')
+}
+
+function mostrarMenu($event){
+    //identificar o elemento menu
+    let menu = document.getElementById("menu")
+    //mudar a visualização do menu
+    menu.style.display = "flex"
+    event.preventDefault()
+}
